98deee49e256837d0f0550fa8e5e2c11550d800a
Jared Lyvers <jared.lyvers@ldnddev.com>
2019-07-02 14:31:26 -0500

    updating field labels

--- assets/js/ldnddev_accordion.js
+++ assets/js/ldnddev_accordion.js
@@ -1,16 +1,10 @@
-var ldnddev_accordion__header = document.getElementsByClassName("ldnddev-accordion__header");
-var i;
-
-for (i = 0; i < ldnddev_accordion__header.length; i++) {
-    ldnddev_accordion__header[i].addEventListener("click", function() {        
-        this.parentNode.classList.toggle("-active");
-        this.classList.toggle("-active");
-        var copy = this.nextElementSibling;    
-        if (copy.style.maxHeight){
-            copy.style.maxHeight = null;
-        }
-        else {
-            copy.style.maxHeight = copy.scrollHeight + "px";      
-        } 
+jQuery(document).ready(function($){
+    $('.ldnddev-accordion').on('click', '.ldnddev-accordion__header', function() {
+        $(this)
+            .toggleClass('-active')
+            .next()
+            .slideToggle()
+            .parent()
+            .toggleClass('-active')
     });
-}
+});
